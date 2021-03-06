8818807f9f9b92c75f4a868e6dd78d68c6eaa6b6
Binny V A <Binny V A>
2021-03-06 17:47:24 +0530

    Data Filled Reports show multiple Shelters / Cities together

--- data_filled.php
+++ data_filled.php
@@ -9,31 +9,54 @@ $sql_checks = $checks;
 $year = findYear($opts['to']);
 $page_title = 'Data Filled';
 
-$data = array('total_classes' => 0, 'teacher_data_filled' => 0, 'student_data_filled' => 0);
+$data_template = array('total_classes' => 0, 'teacher_data_filled' => 0, 'student_data_filled' => 0);
+$data = [];
 
 $sql_checks['not_city_id'] = "Ctr.city_id NOT IN (26, 28)";
 
-$qry = "SELECT DISTINCT C.id, C.status, C.level_id,C.batch_id, C.class_on, Ctr.city_id, B.center_id, SC.id AS student_class_id,  UC.status AS user_class_status
-		FROM Class C
-		INNER JOIN Batch B ON B.id=C.batch_id
-		INNER JOIN Level L ON L.id=C.level_id
-		INNER JOIN Center Ctr ON B.center_id=Ctr.id
-		LEFT JOIN UserClass UC ON UC.class_id=C.id
-		LEFT JOIN StudentClass SC ON SC.class_id=C.id
-		WHERE B.year=$year AND B.status='1' AND L.status='1' AND L.year=$year AND "
-		. implode(' AND ', $sql_checks) . " ORDER BY C.class_on DESC";
-$all_classes = $sql->getAll($qry);
-
-$done = [];
-foreach($all_classes as $class) {
-	if(isset($done[$class['id']])) continue; // To remove duplicate class from being counted again.
-	$done[$class['id']] = true;
-
-	$data['total_classes'] ++;
-	if($class['user_class_status'] != 'projected') $data['teacher_data_filled'] ++; // UserClass.status is not 'projected' - means that mentor has entered data.
-
-	if($class['status'] == 'cancelled') $data['student_data_filled']++; // Class canceled - consider teacher data as entered.
-	else if($class['student_class_id']) $data['student_data_filled']++; // We got a StudentClass.id - so teacher has entered data.
-
+// Get the entire Shelter hirarchy
+$all_centers = $sql->getById("SELECT id, name, city_id FROM Center WHERE status='1' ORDER BY name");
+$city_centers = $sql->getById("SELECT id, name, '' AS centers FROM City WHERE id NOT IN (26, 28) ORDER BY name");
+foreach ($city_centers as $cty_id => $cty) {
+	$centers_in_this_city = array_filter($all_centers, function ($cntr) use ($cty_id) { return $cntr['city_id'] == $cty_id; });
+	$city_centers[$cty_id]['centers'] = $centers_in_this_city;
 }
+
+unset($opts['checks']);
+list($data, $cache_key) = getCacheAndKey('data', $opts);
+
+$cache_status = true;
+if(!$data) {
+	$cache_status = false;
+
+	$qry = "SELECT DISTINCT C.id, C.status, C.level_id,C.batch_id, C.class_on, Ctr.city_id, B.center_id, SC.id AS student_class_id,  UC.status AS user_class_status
+			FROM Class C
+			INNER JOIN Batch B ON B.id=C.batch_id
+			INNER JOIN Level L ON L.id=C.level_id
+			INNER JOIN Center Ctr ON B.center_id=Ctr.id
+			LEFT JOIN UserClass UC ON UC.class_id=C.id
+			LEFT JOIN StudentClass SC ON SC.class_id=C.id
+			WHERE B.year=$year AND B.status='1' AND L.status='1' AND L.year=$year AND "
+			. implode(' AND ', $sql_checks) . " ORDER BY C.class_on DESC";
+	$all_classes = $sql->getAll($qry);
+
+	$done = [];
+	foreach($all_classes as $class) {
+		if(isset($done[$class['id']])) continue; // To remove duplicate class from being counted again.
+		$done[$class['id']] = true;
+
+		if(!isset($data[$class['center_id']])) {
+			$data[$class['center_id']] = $data_template;
+		}
+
+		$data[$class['center_id']]['total_classes'] ++;
+		if($class['user_class_status'] != 'projected') $data[$class['center_id']]['teacher_data_filled'] ++; // UserClass.status is not 'projected' - means that mentor has entered data.
+
+		if($class['status'] == 'cancelled') $data[$class['center_id']]['student_data_filled']++; // Class canceled - consider teacher data as entered.
+		else if($class['student_class_id']) $data[$class['center_id']]['student_data_filled']++; // We got a StudentClass.id - so teacher has entered data.
+	}
+
+	setCache($cache_key, $data);
+}
+
 render();
--- templates/data_filled.php
+++ templates/data_filled.php
@@ -2,27 +2,60 @@
 
 <?php 
 include('_filter.php');
-$percentage_colors = array('#e74c3c', '#16a085'); // Red , Green
-?>
 
-<h3>Total Classes: <?php echo $data['total_classes'] ?></h3>
+// Find the total numbers for all the centers.
+$total = array_reduce($data, function($carry, $cntr) {
+	if(!isset($cntr['total_classes'])) return $carry; 
+
+	$carry['total_classes'] += $cntr['total_classes'];
+	$carry['student_data_filled'] += $cntr['student_data_filled'];
+	$carry['teacher_data_filled'] += $cntr['teacher_data_filled'];
+
+	return $carry;
+}, $data_template);
+
+show($total);
+echo "<hr />";
+
+foreach($city_centers as $cty_id => $city) {
+	if($city_id and $city_id != $cty_id) continue;
+
+	echo "<h1>{$city['name']}</h1>";
+	foreach($city['centers'] as $cntr_id => $cntr) {
+		if($center_id and $center_id != $cntr_id) continue;
+
+		if(!isset($data[$cntr_id])) continue;
+		echo "<h2>{$cntr['name']}</h2>";
+		show($data[$cntr_id]);
+	}
+	echo "<hr />";
+}
+
+
+function show($filled) { 
+	if(!$filled) return;
+	$percentage_colors = array('#e74c3c', '#16a085'); // Red , Green
+	?>
+<h3>Total Classes: <?php echo $filled['total_classes'] ?></h3>
 
 <h4>Teacher Attendance Data Filled: <?php 
-echo $data['teacher_data_filled'];
-$percentage = round($data['teacher_data_filled'] / $data['total_classes'] * 100);
+echo $filled['teacher_data_filled'];
+$percentage = round($filled['teacher_data_filled'] / $filled['total_classes'] * 100);
 ?></h4>
-<p>This data is entered by the mentor</p>
+<!-- <p>This data is entered by the mentor</p> -->
 <div class="progress">
-<div class="data" style="width:<?php echo $percentage ?>%; background-color: <?php echo $percentage_colors[1] ?>;">Data Filled Percentage: <?php echo $percentage ?>%</div>
+<div class="data" style="width:<?php echo $percentage ?>%; background-color: <?php echo $percentage_colors[1] ?>;"><?php echo $percentage ?>% Data Filled</div>
 <div class="no-data" style="width:<?php echo 100-$percentage ?>%; background-color: <?php echo $percentage_colors[0] ?>;">&nbsp;</div>
 </div>
 
 <h4>Student Attendance Data Filled: <?php 
-echo $data['student_data_filled'];
-$percentage = round($data['student_data_filled'] / $data['total_classes'] * 100);
+echo $filled['student_data_filled'];
+$percentage = round($filled['student_data_filled'] / $filled['total_classes'] * 100);
 ?></h4>
-<p>This data is entered by the teacher</p>
+<!-- <p>This data is entered by the teacher</p> -->
 <div class="progress">
-<div class="data" style="width:<?php echo $percentage ?>%; background-color: <?php echo $percentage_colors[1] ?>;">Data Filled Percentage: <?php echo $percentage ?>%</div>
+<div class="data" style="width:<?php echo $percentage ?>%; background-color: <?php echo $percentage_colors[1] ?>;"><?php echo $percentage ?>% Data Filled</div>
 <div class="no-data" style="width:<?php echo 100-$percentage ?>%; background-color: <?php echo $percentage_colors[0] ?>;">&nbsp;</div>
 </div>
+<?php
+}
